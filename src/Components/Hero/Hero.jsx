86295f460ba977dcdc2ac07e25130f90ee70c939
Ricardo Alvarado <hrkalvarado@gmail.com>
2023-10-22 19:16:54 -0600

    creando rutas, navbar y home

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+import hero_imae from "../Assets/hero_image.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>Collection</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-lates-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_imae} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
